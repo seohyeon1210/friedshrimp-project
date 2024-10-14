fd4a729ccd79269b2f1f1f94d75e6d22bbf85ef7
용준 <victoriagrape@naver.com>
2024-10-14 13:32:54 +0900

    crew

--- crewjs/crewmodal.script.js
+++
@@ -1,8 +0,0 @@
-   // JavaScript로 수정 기능 구현
-    function editText() {
-    // 수정할 텍스트 선택
-    const textElement = document.getElementById("text");
-
-    // 새로운 텍스트로 변경
-    textElement.innerHTML = "수정된 텍스트입니다!";
-}
