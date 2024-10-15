b02d7dd4a8eb894cb318606376abe9a94ce6a9a1
JinBaeMoon <wlsqo09@naver.com>
2024-10-15 17:27:01 +0900

    Hooooooooooooooo

---
+++ admin-mainhome/admin-mainhome.js
@@ -0,0 +1,15 @@
+document.getElementById('member-management').addEventListener('click', function() {
+    window.location.href = 'admin-membermmt/admin-membermmt.html'; // 회원관리 페이지 URL
+});
+
+document.getElementById('crew-recruitment').addEventListener('click', function() {
+    window.location.href = 'http://192.168.0.73:5500/admin-crewmmt/admin-crewmmt..html'; // 크루 모집 관리 페이지 URL
+});
+
+document.getElementById('tips-management').addEventListener('click', function() {
+    window.location.href = 'http://192.168.0.73:5500/admin-tipmmt/admin-tipmmt.html'; // 알알꿀팁 관리 페이지 URL
+});
+
+document.getElementById('advertisement-management').addEventListener('click', function() {
+    window.location.href = 'http://192.168.0.73:5500/admin-admmt/admin-admmt.html'; // 광고물 관리 페이지 URL
+});
